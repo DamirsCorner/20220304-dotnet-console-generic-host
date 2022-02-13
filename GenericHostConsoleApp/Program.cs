c54daa7f10bc0221e2207435c1a60b915fc0c679
Damir Arh <damir.arh@gmail.com>
2022-02-13 11:46:10 +0100

    Use CommandLineUtils with generic host

--- GenericHostConsoleApp/Program.cs
+++ GenericHostConsoleApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GenericHostConsoleApp.Commands;
+using GenericHostConsoleApp.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+
+Host.CreateDefaultBuilder()
+    .ConfigureServices((context, services) =>
+    {
+        services.AddOptions<Config>()
+            .Bind(context.Configuration.GetSection(nameof(Config)))
+            .ValidateDataAnnotations();
+
+        services
+            .AddSingleton(provider => provider
+                .GetRequiredService<IOptionsSnapshot<Config>>()
+                .Value);
+    })
+    .RunCommandLineApplicationAsync<AppCommand>(args);
